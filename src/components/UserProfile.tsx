684c9118267ab5439bbe3d9e957d90d9779279ad
Eric Kobby Mensah <mensaheric743@hotmail.com>
2021-05-14 12:14:04 +0000

    component fixes

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -5,15 +5,14 @@ const UserProfile: FC = () => {
 
     const { user } = useAuth0()
     
-    const { name,email,picture } = user
     return (
         <div className="container">
             <div className="image-holder">
-                <img src={picture} width={"200"} height={"200"} alt="profile-avatar" />
+                <img src={user?.picture} width={"200"} height={"200"} alt="profile-avatar" />
             </div>
             <div className="details">
-                <div><strong>Name: </strong>{name}</div><br />
-                <div><strong>Email: </strong>{email}</div>
+                <div><strong>Name: </strong>{user?.name}</div><br />
+                <div><strong>Email: </strong>{user?.email}</div>
             </div>
         </div>
     )
